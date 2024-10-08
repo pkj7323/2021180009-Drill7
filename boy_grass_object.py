c73cd0b27c523c29210b66b0b466ae0f89029b82
leopark <pkj7323@naver.com>
2024-10-08 10:17:15 +0900

    아무것도 없지만 월드를 시뮬레이션하는 구조

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -11,12 +11,37 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
-open_canvas()
 
+def reset_world():
+    global running
+    running = True
+
+
+def update_world():
+    pass
+
+
+def render_world():
+    clear_canvas()
+    update_world()
+
+open_canvas()
+running = True
 # initialization code
+reset_world() # 초기화
+
 
 # game main loop code
 
-# finalization code
+while running:
+    handle_events()
+    update_world() #상호작용을 시뮬레이션
+    render_world() # 그결과를 보여줌
+    delay(0.05)
 
+
+# finalization code
 close_canvas()
+
+
+
