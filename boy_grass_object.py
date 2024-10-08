135efbde669e3e70d84d8d62a1d872df5116c19c
leopark <pkj7323@naver.com>
2024-10-08 10:50:01 +0900

    기본적인 게임엔진 모든 게임은 이런 구조들로 돌아간다

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,30 @@
+import random
+
 from pico2d import *
 
 # Game object class here
+class Grass:
+    # 생성자 만들기
+    def __init__(self):
+        # self는 모양 없는 납작한 붕어빵
+        # 이제 이걸 모양을 잡아줌
+        self.image = load_image("grass.png")
+    def update(self):
+        pass
+    def draw(self):
+        self.image.draw(400,30)
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image("run_animation.png")
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0 , 100, 100, self.x, self.y)
+
 
 def handle_events():
     global running
@@ -14,19 +38,31 @@ def handle_events():
 
 def reset_world():
     global running
+    global world
+    world = []
     running = True
+    grass = Grass()
+    world.append(grass)
+    team = [Boy() for i in range(11)]
+    world += team
 
 
 def update_world():
-    pass
-
+    for obj in world:
+        obj.update()
 
 def render_world():
     clear_canvas()
-    update_world()
+    for obj in world:
+        obj.draw()
+    update_canvas()
+
+
+
 
 open_canvas()
 running = True
+
 # initialization code
 reset_world() # 초기화
 
@@ -35,7 +71,7 @@ reset_world() # 초기화
 
 while running:
     handle_events()
-    update_world() #상호작용을 시뮬레이션
+    update_world() # 상호작용을 시뮬레이션
     render_world() # 그결과를 보여줌
     delay(0.05)
 
